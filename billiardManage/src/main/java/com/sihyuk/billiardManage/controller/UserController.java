5f5f24564a32e507c71db1697f8bd49f31c85f74
inhyuks <sih2341@naver.com>
2019-06-20 10:37:13 +0900

    Controller 수정
    - BilliardController 유저등록기능 제거
    +UserController 추가 유저등록기능 추가

--- billiardManage/src/main/java/com/sihyuk/billiardManage/controller/UserController.java
+++ billiardManage/src/main/java/com/sihyuk/billiardManage/controller/UserController.java
@@ -1,30 +1,24 @@
 package com.sihyuk.billiardManage.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sihyuk.billiardManage.dto.User;
 import com.sihyuk.billiardManage.service.UserService;
 
 @Controller
-public class BilliardController {
-
+public class UserController {
+	
 	@Autowired
 	UserService userService;
 	
-	@RequestMapping(value ="/home" , method = {RequestMethod.GET, RequestMethod.POST})
-	public String initMain() {
-		return "index";
+	@PostMapping(path = "/userpost")
+	public String write(@ModelAttribute User user) {
+		userService.postUser(user);
+		return "redirect:home";
 	}
-	
-	@GetMapping(path = "/regist")
-	public String registPage() {
-		return "regist";
-	}
-
 }
